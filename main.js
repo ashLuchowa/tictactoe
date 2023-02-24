cea951a7112089bf53e1b641ee31235011e47802
Ash Luchowa <ashwinluchowa@gmail.com>
2023-02-24 15:58:17 +0800

    Create gameBoard object + content array

---
+++ main.js
@@ -0,0 +1,7 @@
+//Gameboard object
+const gameBoard = (() => {
+  let gameBoardContent = ["X", "X", "O", 'O', 'X', 'O'];
+  return { gameBoardContent };
+})();
+
+console.log(gameBoard.gameBoardContent);
