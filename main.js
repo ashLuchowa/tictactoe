65d9668196e98f0522f245baa8020d16df5c6b93
Ash Luchowa <ashwinluchowa@gmail.com>
2023-02-27 15:10:51 +0800

    Push player selection into array

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //GameStart Module
 const gameStart = (() => {
-  let gameBoardContent = ["X", "X", "O", "O", "X", "O"];
+  let gameBoardContent = [];
   const player1 = "X";
   const player2 = "O";
   let currentPlayer = player1;
@@ -9,13 +9,18 @@ const gameStart = (() => {
   const box = document.querySelector(".mainboard");
   const singleBox = document.querySelectorAll(".mainboard div");
 
+  //Input playerSelection
   singleBox.forEach((square) => {
     square.addEventListener("click", () => {
       if (currentPlayer === player1) {
         square.textContent = player1;
+        gameBoardContent.push(player1);
+        console.log(gameBoardContent);
         currentPlayer = player2;
       } else if (currentPlayer === player2) {
         square.textContent = player2;
+        gameBoardContent.push(player2);
+        console.log(gameBoardContent);
         currentPlayer = player1;
       }
     });
