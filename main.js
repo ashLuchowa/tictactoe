e9bddb04f31da4d9e076fe87dcafafdf1dc8405d
Ash Luchowa <ashwinluchowa@gmail.com>
2023-02-27 14:26:36 +0800

    Make each square clickable

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
-//Gameboard object
+//Gameboard Module
 const gameBoard = (() => {
   let gameBoardContent = ["X", "X", "O", "O", "X", "O"];
-  const player1 = "X";
-  const player2 = "O";
   const gameOver = false;
-  return { gameBoardContent, player1, player2, gameOver };
+  return { gameBoardContent, gameOver };
 })();
 
-console.log(gameBoard);
+//GameStart Module
+const gameStart = (() => {
+  const player1 = "X";
+  const player2 = "O";
+
+  const box = document.querySelector(".mainboard");
+  const singleBox = document.querySelectorAll(".mainboard div");
+
+  singleBox.forEach((square) => {
+    square.addEventListener("click", () => {
+      square.textContent = player1;
+      //
+    });
+  });
+
+  return { box, singleBox, player1, player2 };
+})();
+
+console.log(gameStart.player1);
