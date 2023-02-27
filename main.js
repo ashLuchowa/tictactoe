239fd4cae6fbc29c28e9abddd88d978e29b45676
Ash Luchowa <ashwinluchowa@gmail.com>
2023-02-27 21:06:37 +0800

    Convert players into objects

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+//Default Player Object
+const players = (playerName, playerScore) => {
+  return { playerName, playerScore };
+};
+
 //GameStart Module
 const gameStart = (() => {
   let gameBoardContent = [];
-  const player1 = "X";
-  const player2 = "O";
+  const player1 = players("X", 0);
+  const player2 = players("O", 0);
   let currentPlayer = player1;
   const gameOver = false;
 
@@ -14,16 +19,16 @@ const gameStart = (() => {
     square.addEventListener("click", () => {
       if (currentPlayer === player1) {
         if (!square.textContent) {
-          square.textContent = player1;
-          gameBoardContent.push(player1);
+          square.textContent = player1.playerName;
+          gameBoardContent.push(player1.playerName);
           console.log(gameBoardContent);
           console.log({ currentPlayer });
           currentPlayer = player2;
         }
       } else if (currentPlayer === player2) {
         if (!square.textContent) {
-          square.textContent = player2;
-          gameBoardContent.push(player2);
+          square.textContent = player2.playerName;
+          gameBoardContent.push(player2.playerName);
           console.log(gameBoardContent);
           console.log({ currentPlayer });
           currentPlayer = player1;
