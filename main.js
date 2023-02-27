c50e6dfbac7b0fc1cab552184b99a25d266d1b15
Ash Luchowa <ashwinluchowa@gmail.com>
2023-02-27 15:29:28 +0800

    Prevent multi input in same location

--- main.js
+++ main.js
@@ -13,15 +13,21 @@ const gameStart = (() => {
   singleBox.forEach((square) => {
     square.addEventListener("click", () => {
       if (currentPlayer === player1) {
-        square.textContent = player1;
-        gameBoardContent.push(player1);
-        console.log(gameBoardContent);
-        currentPlayer = player2;
+        if (!square.textContent) {
+          square.textContent = player1;
+          gameBoardContent.push(player1);
+          console.log(gameBoardContent);
+          console.log({ currentPlayer });
+          currentPlayer = player2;
+        }
       } else if (currentPlayer === player2) {
-        square.textContent = player2;
-        gameBoardContent.push(player2);
-        console.log(gameBoardContent);
-        currentPlayer = player1;
+        if (!square.textContent) {
+          square.textContent = player2;
+          gameBoardContent.push(player2);
+          console.log(gameBoardContent);
+          console.log({ currentPlayer });
+          currentPlayer = player1;
+        }
       }
     });
   });
