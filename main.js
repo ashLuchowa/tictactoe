df3f6468bcfb0af81483b9b83558157daa610998
Ash Luchowa <ashwinluchowa@gmail.com>
2023-03-10 14:22:15 +0800

    reset from scratch + keeps objects + improve board array

--- main.js
+++ main.js
@@ -5,43 +5,21 @@ const players = (playerName, playerScore) => {
 
 //GameStart Module
 const gameStart = (() => {
-  let gameBoardContent = [];
+  let gameBoardContent = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
   const player1 = players("X", 0);
   const player2 = players("O", 0);
   let currentPlayer = player1;
   let gameRound = 0;
   const gameOver = false;
 
-  const box = document.querySelector(".mainboard");
-  const singleBox = document.querySelectorAll(".mainboard div");
-
-  //Input playerSelection
-  singleBox.forEach((square) => {
-    square.addEventListener("click", () => {
-      if (currentPlayer === player1) {
-        if (!square.textContent) {
-          square.textContent = player1.playerName;
-          gameBoardContent.push(player1.playerName);
-          console.log(gameBoardContent);
-          console.log({ currentPlayer });
-          currentPlayer = player2;
-        }
-      } else if (currentPlayer === player2) {
-        if (!square.textContent) {
-          square.textContent = player2.playerName;
-          gameBoardContent.push(player2.playerName);
-          console.log(gameBoardContent);
-          console.log({ currentPlayer });
-          currentPlayer = player1;
-        }
-      }
-    });
-  });
+  console.log(gameBoardContent[0][0]);
 
   return {
     gameBoardContent,
-    box,
-    singleBox,
     player1,
     player2,
     currentPlayer,
