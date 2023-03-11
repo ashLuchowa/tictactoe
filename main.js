7a63f04db34e6f0c0dedf55edc780eb646d3d596
Ash Luchowa <ashwinluchowa@gmail.com>
2023-03-11 10:31:50 +0800

    Reduce repeated codes into function

--- main.js
+++ main.js
@@ -16,16 +16,13 @@ const gameStart = (() => {
   let gameRound = 0;
   const gameOver = false;
 
-  //Each square variables
-  const square1 = document.getElementById("square1");
-  const square2 = document.getElementById("square2");
-  const square3 = document.getElementById("square3");
-  const square4 = document.getElementById("square4");
-  const square5 = document.getElementById("square5");
-  const square6 = document.getElementById("square6");
-  const square7 = document.getElementById("square7");
-  const square8 = document.getElementById("square8");
-  const square9 = document.getElementById("square9");
+  //click square event
+  const setSquareClick = (squareId, row, col) => {
+    const square = document.getElementById(squareId);
+    square.addEventListener("click", () => {
+      handleSquareClick(square, row, col);
+    });
+  };
 
   //what happens for each square Function
   const handleSquareClick = (squareElement, row, col) => {
@@ -38,33 +35,19 @@ const gameStart = (() => {
   };
 
   //click events
-  square1.addEventListener('click', () => {
-    handleSquareClick(square1, 0, 0);
-  })
-  square2.addEventListener('click', () => {
-    handleSquareClick(square2, 0, 1);
-  })
-  square3.addEventListener('click', () => {
-    handleSquareClick(square3, 0, 2);
-  })
-  square4.addEventListener('click', () => {
-    handleSquareClick(square4, 1, 0);
-  })
-  square5.addEventListener('click', () => {
-    handleSquareClick(square5, 1, 1);
-  })
-  square6.addEventListener('click', () => {
-    handleSquareClick(square6, 1, 2);
-  })
-  square7.addEventListener('click', () => {
-    handleSquareClick(square7, 2, 0);
-  })
-  square8.addEventListener('click', () => {
-    handleSquareClick(square8, 2, 1);
-  })
-  square9.addEventListener('click', () => {
-    handleSquareClick(square9, 2, 2);
-  })
+  const setSquareClickEvents = () => {
+    setSquareClick("square1", 0, 0);
+    setSquareClick("square2", 0, 1);
+    setSquareClick("square3", 0, 2);
+    setSquareClick("square4", 1, 0);
+    setSquareClick("square5", 1, 1);
+    setSquareClick("square6", 1, 2);
+    setSquareClick("square7", 2, 0);
+    setSquareClick("square8", 2, 1);
+    setSquareClick("square9", 2, 2);
+  };
+
+  setSquareClickEvents();
 
   return {
     gameBoardContent,
@@ -74,14 +57,7 @@ const gameStart = (() => {
     gameOver,
     gameRound,
     handleSquareClick,
-    square1,
-    square2,
-    square3,
-    square4,
-    square5,
-    square6,
-    square7,
-    square8,
-    square9,
+    setSquareClick,
+    setSquareClickEvents,
   };
 })();
